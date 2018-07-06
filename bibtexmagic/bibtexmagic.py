dd975f7ef73e2f228e47598b4b1900c166d95ba0
piotrbajger <piotr.bajger@hotmail.com>
2018-07-06 18:01:22 +0200

    Field parsing for Authors, Title and Pages

--- bibtexmagic/bibtexmagic.py
+++ bibtexmagic/bibtexmagic.py
@@ -1,12 +1,14 @@
 import warnings
+import sys
 import re
+import json
 
 class BibTexMagic():
     regular_types = ['article', 'book']
 
-    def __init__(self):
-        self.entries = [];
-
+    def __init__(self, pages_double_hyphened=True):
+        self.entries = []
+        self.pages_double_hyphened = pages_double_hyphened
 
     def parse_bib(self, filename):
         with open(filename) as bibfile:
@@ -28,6 +30,47 @@ class BibTexMagic():
 
             if entry is not None:
                 self.entries.append(entry)
+
+    def parse_field(self, field_name, field_value):
+        print(field_name)
+        if field_name == "author":
+            return self.parse_field_author(field_value)
+        elif field_name == "title":
+            return self.parse_field_title(field_value)
+        elif field_name == "pages":
+            return self.parse_field_pages(field_value)
+
+        return field_value
+
+
+    def parse_field_author(self, field_value):
+        author_list = field_value.split(" and ")
+
+        #TBA: Parse von, Jr, etc.. Make sure the names are
+        #in the A.B. Surname, {prefix} format
+
+        return author_list
+
+    def parse_field_title(self, field_value):
+        par = self.get_parentheses(field_value)
+
+        to_return = ""
+        pos = 0
+        for key in sorted(par):
+            to_return += field_value[pos:key].lower()
+            to_return += field_value[(key+1):par[key]]
+            pos = par[key]+1
+
+        to_return += field_value[pos:]
+        to_return = to_return[0].upper() + to_return[1:]
+
+        return to_return
+
+    def parse_field_pages(self, field_value):
+        if self.pages_double_hyphened:
+            return re.sub("-{1,2}", "--", field_value)
+
+        return field_value
 
     def parse_entry(self, entry_raw):
         end_type = entry_raw.find('{')
@@ -53,24 +96,45 @@ class BibTexMagic():
 
                 field_start = prev_end + find_field.start()
 
-
                 field_name_end = entry_raw.find('=', field_start)
 
                 field_name = entry_raw[field_start:field_name_end].strip()
 
-                field_val_start = entry_raw.find('{', field_name_end)
+                field_val_start = entry_raw.find('{', field_name_end) + 1
                 field_val_end = entry_raw.find('},', field_val_start)
 
-                field_val = entry_raw[(field_val_start+1):field_val_end]
+                field_parsed = self.parse_field(field_name, entry_raw[field_val_start:field_val_end])
+
+                field_val = field_parsed
+
                 prev_end = field_val_end
 
                 entry[field_name] = field_val
 
         return entry
 
+
+    def get_parentheses(self, s):
+        to_return = {}
+        pstack = []
+
+        for i, c in enumerate(s):
+            if c == '{':
+                pstack.append(i)
+            elif c == '}':
+                if not pstack:
+                    raise IndexError("No matching opening parenthesis for " + str(i))
+                to_return[pstack.pop()] = i
+
+        if pstack:
+            raise IndexError("No matching closing parenthesis for " + str(pstack.pop()))
+
+        return to_return
+
+
 if __name__ == "__main__":
     parser = BibTexMagic()
 
     parser.parse_bib('bib.bib')
 
-    print(parser.entries)
+    json.dump(parser.entries, sys.stdout, indent=4, separators=(',', ': '))
