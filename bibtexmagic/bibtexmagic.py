ad9ddb08739e1a1b09f26272a7b231632caa21f5
piotrbajger <piotr.bajger@hotmail.com>
2018-08-04 21:16:19 +0200

    Added conversion between LaTeX and unicode. Added tests.

--- bibtexmagic/bibtexmagic.py
+++ bibtexmagic/bibtexmagic.py
@@ -3,12 +3,24 @@ import sys
 import re
 import json
 
-class BibTexMagic():
-    regular_types = ['article', 'book']
+from latextouni import LatexToUni
 
-    def __init__(self, pages_double_hyphened=True):
+
+class BibTexMagic():
+    ALLOWED_ENTRIES = ['article', 'book']
+    ALLOWED_FIELDS = ['address', 'annote', 'author', 'booktitle', 'chapter',
+        'crossref', 'edition', 'editor', 'file', 'howpublished', 'institution', 'journal',
+        'key', 'month', 'note', 'number', 'organization', 'pages', 'publisher',
+        'school', 'series', 'title', 'type', 'volume', 'year']
+
+    def __init__(self, pages_double_hyphened=True, latex_to_unicode=True, restrict_to_allowed=True):
         self.entries = []
         self.pages_double_hyphened = pages_double_hyphened
+        self.restrict_to_allowed = restrict_to_allowed
+
+        if latex_to_unicode:
+            self.latex_to_unicode = True
+            self.converter = LatexToUni()
 
     def parse_bib(self, filename):
         with open(filename) as bibfile:
@@ -31,8 +43,53 @@ class BibTexMagic():
             if entry is not None:
                 self.entries.append(entry)
 
+
+    def parse_entry(self, entry_raw):
+        end_type = entry_raw.find('{')
+        entry_type = entry_raw[1:end_type].lower()
+
+        if self.restrict_to_allowed or entry_type not in self.ALLOWED_ENTRIES:
+            warnings.warn('Entry type {} not supported.'.format(entry_type))
+            return None
+
+        entry = {}
+
+        if entry_type == 'article':
+            end_key = entry_raw.find(',', end_type+1)
+            entry['key'] = entry_raw[(end_type+1):end_key]
+
+            prev_end = end_key
+
+            while True:
+                find_field = re.search('\w', entry_raw[prev_end:])
+
+                if find_field is None:
+                    break
+
+                field_start = prev_end + find_field.start()
+
+                field_name_end = entry_raw.find('=', field_start)
+
+                field_name = entry_raw[field_start:field_name_end].strip()
+
+                field_val_start = entry_raw.find('{', field_name_end) + 1
+                field_val_end = entry_raw.find('},', field_val_start)
+
+                field_parsed = self.parse_field(field_name, entry_raw[field_val_start:field_val_end])
+
+                field_val = field_parsed
+
+                prev_end = field_val_end
+
+                if not self.restrict_to_allowed or field_name.lower() in self.ALLOWED_FIELDS:
+                    entry[field_name] = field_val
+                else:
+                    warnings.warn('Field {} not supported'.format(field_name))
+
+        return entry
+
+
     def parse_field(self, field_name, field_value):
-        print(field_name)
         if field_name == "author":
             return self.parse_field_author(field_value)
         elif field_name == "title":
@@ -44,6 +101,9 @@ class BibTexMagic():
 
 
     def parse_field_author(self, field_value):
+        if self.latex_to_unicode:
+            field_value = self.converter.lat_to_uni(field_value)
+
         author_list = field_value.split(" and ")
 
         #TBA: Parse von, Jr, etc.. Make sure the names are
@@ -61,7 +121,7 @@ class BibTexMagic():
             to_return += field_value[(key+1):par[key]]
             pos = par[key]+1
 
-        to_return += field_value[pos:]
+        to_return += field_value[pos:].lower()
         to_return = to_return[0].upper() + to_return[1:]
 
         return to_return
@@ -71,47 +131,6 @@ class BibTexMagic():
             return re.sub("-{1,2}", "--", field_value)
 
         return field_value
-
-    def parse_entry(self, entry_raw):
-        end_type = entry_raw.find('{')
-        entry_type = entry_raw[1:end_type].lower()
-
-        if entry_type not in self.regular_types:
-            warnings.warn('Entry type \'' + entry_type + '\' not supported.')
-            return None
-
-        entry = {}
-
-        if entry_type == 'article':
-            end_key = entry_raw.find(',', end_type+1)
-            entry['key'] = entry_raw[(end_type+1):end_key]
-
-            prev_end = end_key
-
-            while True:
-                find_field = re.search('\w', entry_raw[prev_end:])
-
-                if find_field is None:
-                   break
-
-                field_start = prev_end + find_field.start()
-
-                field_name_end = entry_raw.find('=', field_start)
-
-                field_name = entry_raw[field_start:field_name_end].strip()
-
-                field_val_start = entry_raw.find('{', field_name_end) + 1
-                field_val_end = entry_raw.find('},', field_val_start)
-
-                field_parsed = self.parse_field(field_name, entry_raw[field_val_start:field_val_end])
-
-                field_val = field_parsed
-
-                prev_end = field_val_end
-
-                entry[field_name] = field_val
-
-        return entry
 
 
     def get_parentheses(self, s):
@@ -131,9 +150,12 @@ class BibTexMagic():
 
         return to_return
 
+    def convert_to_utf(self, s):
+        pass
 
+#Keep it here for now for testing purposes
 if __name__ == "__main__":
-    parser = BibTexMagic()
+    parser = BibTexMagic(restrict_to_allowed=False)
 
     parser.parse_bib('bib.bib')
 
---
+++ tests/test_LatexToUnicode.py
@@ -0,0 +1,26 @@
+import unittest
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+print(sys.path[0])
+
+from bibtexmagic.bibtexmagic.latextouni import LatexToUni
+
+class LatexToUnicodeTest(unittest.TestCase):
+    def test_latex_to_uni(self):
+        converter = LatexToUni()
+
+        for uni, lat in converter._UNI2LAT.items():
+            #The two replaces below allow to go back and forth between
+            #usual string escaping and escapes necessary for the regex
+            #to work
+            lat = lat.replace('\\^', '^').replace('\\\\', '\\')
+            self.assertTrue(uni == converter.lat_to_uni(lat))
+
+    def test_uni_to_latex(self):
+        converter = LatexToUni()
+
+        for uni, lat in converter._UNI2LAT.items():
+            self.assertTrue(lat == converter.uni_to_lat(uni))
