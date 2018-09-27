a40fe085ebfb1ade570dbda7f5980316221d3eb4
piotrbajger <piotr.bajger@hotmail.com>
2018-09-27 23:06:16 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='BibTeXMagic',
+    version='0.1alpha',
+    author='Piotr Bajger',
+    author_email='piotr.bajger@hotmail.com',
+    packages=['bibtexmagic'],
+    url='https://gitlab.com/piotrbajger/bibtexmagic',
+    license='LICENSE.txt',
+    description='BibTeX parser in Python.',
+    long_description=open('README.md').read(),
+)
