ecbe9e602d26dc1422e2a5ab5fc2713d4dfa74c3
piotrbajger <piotr.bajger@hotmail.com>
2018-09-29 01:04:57 +0200

    Fix latex<-->unicode issues.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-#BibTexMagic
+[![pipeline status](https://gitlab.com/piotrbajger/bibtexmagic/badges/master/pipeline.svg)](https://gitlab.com/piotrbajger/bibtexmagic/commits/master)
+
+## BibTexMagic
+
 BibTexMagic is a parser for BibTex.
--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -11,15 +11,31 @@ class LatexToUnicodeTest(unittest.TestCase):
     def test_latex_to_uni(self):
         converter = LatexToUni()
 
-        for uni, lat in converter._UNI2LAT.items():
+        for pair in converter._UNI2LAT:
             #The two replaces below allow to go back and forth between
             #usual string escaping and escapes necessary for the regex
             #to work
-            lat = lat.replace('\\^', '^').replace('\\\\', '\\')
-            self.assertTrue(uni == converter.lat_to_uni(lat))
+            lat  = pair[1].replace('\\^', '^').replace('\\\\', '\\')
+            self.assertTrue(pair[0] == converter.lat_to_uni(lat))
 
     def test_uni_to_latex(self):
         converter = LatexToUni()
 
-        for uni, lat in converter._UNI2LAT.items():
-            self.assertTrue(lat == converter.uni_to_lat(uni))
+        for pair in converter._UNI2LAT:
+            #The two replaces below allow to go back and forth between
+            #usual string escaping and escapes necessary for the regex
+            #to work
+            lat  = pair[1].replace('\\^', '^').replace('\\\\', '\\')
+            self.assertEqual(lat, converter.uni_to_lat(pair[0]))
+
+
+    def test_are_inverses(self):
+        converter = LatexToUni()
+
+        #These two operations should be each others inverses
+        macros = r"\.{z}\'{o}\l\'{c}\l\lambda"
+        macros_uni = converter.lat_to_uni(macros)
+        uni = converter.uni_to_lat(macros_uni)
+
+        self.assertEqual(macros, uni)
+
